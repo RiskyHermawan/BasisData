918d470cec6c68761885fa968e70b55d0bbe6bb2
RiskyHermawan <64541424+RiskyHermawan@users.noreply.github.com>
2020-05-22 23:46:11 +0700

    Update view.php

--- view.php
+++ view.php
@@ -5,13 +5,13 @@ $stid = oci_parse($conn, $sql);
 oci_execute($stid);
 
 while(oci_fetch($stid)){
-	echo oci_result($stid, 'ID_ANGGOTA'), "|";
-	echo oci_result($stid, 'NAMA'), "|";
-	echo oci_result($stid, 'JENIS_IDENTITAS'), "|";
-	echo oci_result($stid, 'NO_IDENTITAS'), "|";
-	echo oci_result($stid, 'ALAMAT'), "|";
-	echo oci_result($stid, 'TTL'), "|";
-	echo oci_result($stid, 'GENDER'), "|";
-	echo oci_result($stid, 'STATUS'), "|";
+	echo oci_result($stid, 'ID_ANGGOTA'), " | ";
+	echo oci_result($stid, 'NAMA'), " | ";
+	echo oci_result($stid, 'JENIS_IDENTITAS'), " | ";
+	echo oci_result($stid, 'NO_IDENTITAS'), " | ";
+	echo oci_result($stid, 'ALAMAT'), " | ";
+	echo oci_result($stid, 'TTL'), " | ";
+	echo oci_result($stid, 'GENDER'), " | ";
+	echo oci_result($stid, 'STATUS'), " | ";
 	echo oci_result($stid, 'TELEPON'), "<br>\n";
 }
